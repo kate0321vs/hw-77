1f68bd61c31ace055c75ddb57b3bb980a0c05aa4
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-03-27 23:13:45 +0600

    Создала и настроила проект

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import {CssBaseline} from "@mui/material";
+import {store} from "./app/store.ts";
+
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <CssBaseline/>
+            <App />
+        </BrowserRouter>
+    </Provider>
+)
